fa4cb4bb5335610f47c6b7fef8b75336a2efbaa6
JesseAidenBolton <jesseabolton@gmail.com>
2023-11-09 17:59:05 +1100

    modified open close on body dialog

--- src/components/SelectBodyDialog.tsx
+++ src/components/SelectBodyDialog.tsx
@@ -18,19 +18,17 @@ type Props = {
     muscle: string,
 };
 
-
-
-
 const SelectBodyDialog = () => {
 
 
-    const [buttons, setButtons] = useState( exerciseList.map((exercise) => {
-            return (
-                <Button key={exercise.id} onClick={() => {handleClick(exercise.muscle)}}>{exercise.muscle}</Button>
-            )
-        }))
+    const defaultMuscles = exerciseList.map((exercise) => {
+        return (
+            <Button key={exercise.id} onClick={() => {handleClick(exercise.muscle)}}>{exercise.muscle}</Button>
+        )})
 
-    const [muscle, setMuscle] = useState('')
+    const [buttons, setButtons] = useState(defaultMuscles)
+
+    const [open, setOpen] = useState(false);
 
 
     const getExercises = (muscle: string) => {
@@ -40,27 +38,30 @@ const SelectBodyDialog = () => {
 
     const handleClick = (bodyType: string) => {
 
-
-
-
         const exercises = getExercises(bodyType)
-
-        console.log(exercises)
 
         setButtons(exercises.map((exercise) => {
             return (
                 <Button key={exercise.id} onClick={() => {handleClick(exercise.exercise)}}>{exercise.exercise}</Button>
             )
         }))
-
-
-
-
     }
+
+    const setDialogOpen = () =>
+    {
+        console.log("changing")
+        setOpen(prev => !prev)
+    }
+
+    useEffect(() => {
+        if(!open){
+            setButtons(defaultMuscles)
+        }
+    }, [open])
 
     return (
         <div>
-        <Dialog>
+        <Dialog open={open} onOpenChange={setDialogOpen}>
             <DialogTrigger>
                 <div className="border-dashed border-2 flex border-amber-600 h-full rounded-lg items-center justify-center sm:flex-col hover:shadow-xl transition hover:-translate-y-1 flex-row p-4">
                     <Plus className="w-6 h-6 text-amber-600" strokeWidth={3} />
@@ -79,12 +80,8 @@ const SelectBodyDialog = () => {
                     </DialogDescription>
                 </DialogHeader>
                 {buttons}
-                {/*{exerciseList.map((exercise) => {
-                    return (
-                        <Button key={exercise.id} onClick={() => {handleClick(exercise.muscle)}}>{exercise.muscle}</Button>
-                    )
-                })}*/}
             </DialogContent>
+
         </Dialog>
 
         </div>
