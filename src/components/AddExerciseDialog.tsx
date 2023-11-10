100191f55f0d337407e53712ab3acb4c1def4e35
JesseAidenBolton <jesseabolton@gmail.com>
2023-11-10 18:18:47 +1100

    refactored dashboard and dialogs

--- src/components/AddExerciseDialog.tsx
+++ src/components/AddExerciseDialog.tsx
@@ -1,38 +1,72 @@
-import {DialogContent, DialogDescription, DialogHeader, DialogTitle} from "@/components/ui/dialog";
+import {useCallback, useEffect, useMemo, useState} from "react";
 
-type Props = {
 
-    muscle: string;
 
-};
-import { exerciseList } from "@/data/exerciseList";
+
 import {Button} from "@/components/ui/button";
+import {Popover, PopoverContent, PopoverTrigger} from "@/components/ui/popover";
+import {Input} from "@/components/ui/input";
+import {Label} from "@/components/ui/label";
+import {Minus, Plus} from "lucide-react";
+
+
+interface AddExerciseDialogProps {
+    isOpen: boolean;
+    onOpenChange: () => void;
+    exercise: string;
+    //onExerciseSelected: (exercise: Exercise) => void;
+    // getExercises: (bodyPart: string) => Exercise[];
+}
+
+
+const AddExerciseDialog: React.FC<AddExerciseDialogProps> = ({ isOpen, onOpenChange, exercise}) => {
+
+    const [sets, setSets] = useState<number>(1);
+
+    const increase = useCallback(() => {
+        setSets((prevSets) => (prevSets < 10 ? prevSets + 1 : prevSets));
+    }, []);
+
+    const decrease = useCallback(() => {
+        setSets((prevSets) => (prevSets > 1 ? prevSets - 1 : prevSets));
+    }, []);
 
 
 
-const AddExerciseDialog = (props: Props) => {
+    //console.log("AddExercise Render")
 
-
-    const exercises = exerciseList.filter(e=> e.muscle === props.muscle)
-
-    console.log(exercises)
+    // Create a list of input elements for sets
+    const setInputs = Array.from({ length: sets }, (_, index) => (
+        <div key={index} className="grid grid-cols-3 items-center gap-4">
+            <Label htmlFor={`set-input-${index}`}>{`Set ${index + 1}`}</Label>
+            <Input
+                id={`set-input-${index}`}
+                defaultValue="10kg"
+                className="col-span-2 h-8"
+            />
+        </div>
+    ));
 
     return (
-        <DialogContent>
-            <DialogHeader>
-                <DialogTitle>
-                    New exercise
-                </DialogTitle>
-                <DialogDescription>
-                    You can add a new exercise by clicking one of the buttons below.
-                </DialogDescription>
-            </DialogHeader>
-            {exerciseList.map((exercise) => {
-                return (
-                    <Button key={exercise.id}>{exercise.muscle}</Button>
-                )
-            })}
-        </DialogContent>
+        <Popover open={isOpen} onOpenChange={onOpenChange}>
+            <PopoverTrigger>
+            </PopoverTrigger>
+            <PopoverContent>
+                <div className="grid gap-4 p-4">
+                    <h4 className="font-medium leading-none">Sets for {exercise}</h4>
+                    <div className="flex justify-between items-center">
+                        <Button onClick={decrease} disabled={sets <= 1} className="bg-amber-500 w-10 h-10 m-2">
+                            <Minus className="w-4 h-4" strokeWidth={3} />
+                        </Button>
+                        <Label>{sets}</Label>
+                        <Button onClick={increase} disabled={sets >= 10} className="bg-amber-500 w-10 h-10 m-2">
+                            <Plus className="w-4 h-4" strokeWidth={3} />
+                        </Button>
+                    </div>
+                    {setInputs}
+                </div>
+            </PopoverContent>
+        </Popover>
     );
 };
 export default AddExerciseDialog;
--- src/components/SelectBodyDialog.tsx
+++ src/components/SelectBodyDialog.tsx
@@ -1,5 +1,3 @@
-
-
 import {
     Dialog,
     DialogContent,
@@ -10,58 +8,22 @@ import {
 } from "@/components/ui/dialog";
 import {Plus} from "lucide-react";
 import {Button} from "@/components/ui/button";
-import AddExerciseDialog from "@/components/AddExerciseDialog";
 import { exerciseList } from "@/data/exerciseList";
-import {useEffect, useState} from "react";
 
-type Props = {
-    muscle: string,
-};
-
-const SelectBodyDialog = () => {
-
-
-    const defaultMuscles = exerciseList.map((exercise) => {
-        return (
-            <Button key={exercise.id} onClick={() => {handleClick(exercise.muscle)}}>{exercise.muscle}</Button>
-        )})
-
-    const [buttons, setButtons] = useState(defaultMuscles)
-
-    const [open, setOpen] = useState(false);
+interface SelectBodyDialogProps {
+    isOpen: boolean;
+    onOpenChange: () => void;
+    onBodyPartSelected: (muscle: string) => void;
+    //exerciseList: Exercise[];
+}
 
 
-    const getExercises = (muscle: string) => {
-        const filter = exerciseList.filter(e => e.muscle === muscle)
-        return filter[0].exercises
-    }
+const SelectBodyDialog: React.FC<SelectBodyDialogProps> = ({ isOpen, onOpenChange, onBodyPartSelected }) => {
 
-    const handleClick = (bodyType: string) => {
-
-        const exercises = getExercises(bodyType)
-
-        setButtons(exercises.map((exercise) => {
-            return (
-                <Button key={exercise.id} onClick={() => {handleClick(exercise.exercise)}}>{exercise.exercise}</Button>
-            )
-        }))
-    }
-
-    const setDialogOpen = () =>
-    {
-        console.log("changing")
-        setOpen(prev => !prev)
-    }
-
-    useEffect(() => {
-        if(!open){
-            setButtons(defaultMuscles)
-        }
-    }, [open])
-
+    console.log("SelectBody Render")
     return (
         <div>
-        <Dialog open={open} onOpenChange={setDialogOpen}>
+        <Dialog open={isOpen} onOpenChange={onOpenChange}>
             <DialogTrigger>
                 <div className="border-dashed border-2 flex border-amber-600 h-full rounded-lg items-center justify-center sm:flex-col hover:shadow-xl transition hover:-translate-y-1 flex-row p-4">
                     <Plus className="w-6 h-6 text-amber-600" strokeWidth={3} />
@@ -79,11 +41,13 @@ const SelectBodyDialog = () => {
                         You can add a new exercise by clicking one of the buttons below.
                     </DialogDescription>
                 </DialogHeader>
-                {buttons}
+                {exerciseList.map((exercise) => (
+                    <Button key={exercise.id} onClick={() => onBodyPartSelected(exercise.muscle)}>
+                        {exercise.muscle}
+                    </Button>
+                ))}
             </DialogContent>
-
         </Dialog>
-
         </div>
     );
 };
