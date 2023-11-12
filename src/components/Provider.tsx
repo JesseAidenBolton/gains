c590f14edd0a86dca125f6ede3534444b62d3f52
JesseAidenBolton <jesseabolton@gmail.com>
2023-11-13 00:12:30 +1100

    added edit ui and some functionality for exercises

---
+++ src/components/Provider.tsx
@@ -0,0 +1,18 @@
+'use client'
+
+import {QueryClient, QueryClientProvider} from "@tanstack/react-query"; [QueryClient]
+
+type Props = {
+    children: React.ReactNode
+}
+
+const queryClient = new QueryClient()
+
+const Provider = ({children}: Props) => {
+    return (
+        <QueryClientProvider client={queryClient}>
+            {children}
+        </QueryClientProvider>
+    );
+};
+export default Provider;
