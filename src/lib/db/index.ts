dc06648012e1273f2fed2164a247795ff4622042
JesseAidenBolton <jesseabolton@gmail.com>
2023-11-16 22:34:16 +1100

    removed modules and reinstall

--- src/lib/db/index.ts
+++ src/lib/db/index.ts
@@ -4,7 +4,7 @@ import {drizzle} from "drizzle-orm/neon-http";
 neonConfig.fetchConnectionCache = true
 
 if(!process.env.DATABASE_URL) {
-    throw new Error('DATABASE_URL is not defined');
+    throw new Error('DATABASE_URL is not defined.');
 }
 
 const sql = neon(process.env.DATABASE_URL);
